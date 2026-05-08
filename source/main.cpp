f129fa4c8c3e0a82d119e43f9b739d56ba037f24
thewierdnut <9004013+thewierdnut@users.noreply.github.com>
2026-05-08 06:18:54 -0500

    Cherry pick upstream PR 12416 to fix crash on reloading dead pilot

--- source/main.cpp
+++ source/main.cpp
@@ -38,7 +38,6 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #include "MainPanel.h"
 #include "MenuPanel.h"
 #include "Panel.h"
-#include "PilotProfile.h"
 #include "PlayerInfo.h"
 #include "Plugins.h"
 #include "Preferences.h"
@@ -228,9 +227,6 @@ int main(int argc, char *argv[])
 			// Set the game's initial internal state.
 			GameData::FinishLoading();
 			CrashState::Set(CrashState::LOADED);
-
-			// Now load all pilot profiles, which may rely on game data.
-			PilotProfile::LoadProfiles();
 
 			// Reference check the universe, as known to the player. If no player found,
 			// then check the default state of the universe.
